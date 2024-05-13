6d894aaa1a86efdc59d19f9142e7de0c79232528
AgusRodriguez15 <8670@holbertonstudents.com>
2024-05-13 00:33:30 +0000

    1 solution

---
+++ debugging/print_arguments.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    """
+    Calcula el factorial de un número entero no negativo.
+    """
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Uso: {} <número>".format(sys.argv[0]))
+        sys.exit(1)
+
+    try:
+        num = int(sys.argv[1])
+        if num < 0:
+            print("El número debe ser no negativo.")
+        else:
+            print("Factorial de {}: {}".format(num, factorial(num)))
+    except ValueError:
+        print("Por favor, ingresa un número entero válido.")
